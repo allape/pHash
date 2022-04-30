2aac01407fe12b85f41fe178b0b0e47e451f90f3
ALLAPE <20764837+allape@users.noreply.github.com>
2022-04-30 23:53:34 +0800

    fix: docker build error

--- docker/install.sh
+++ docker/install.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
 cd pHash || exit 1
+
+# these commands will throw some missing errors
 autoreconf
-aclocal && libtoolize --force && automake --add-missing && autoreconf
+automake --add-missing
+autoreconf
 automake
 
+# generate header file
 cmake configure .
 cmake .
 make clean
 
+# this time will throw version mismatch
+./configure --enable-video-hash=no
+make
+
+# make version match
+aclocal && libtoolize --force && autoconf
+automake --add-missing
+autoreconf
+
+# final build
 ./configure --enable-video-hash=no
 make && make install
+
 cd ..
