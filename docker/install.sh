bb0bc1b12ca0d54d3677bf0d975ab4f1b9de4ef4
as <allensnape@qq.com>
2022-05-15 19:50:21 +0800

    refactor: refine docker build process

--- docker/install.sh
+++ docker/install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-cd pHash || exit 1
+# This script should be run in `pHash` root folder
+
+# `cd` can NOT be used in script, or `libtoolize` will misbehave
+#cd pHash || exit 1
 
 # generate header file
 cmake -DWITH_AUDIO_HASH=1 -DWITH_VIDEO_HASH=1 .
@@ -8,12 +11,6 @@ cmake -DWITH_AUDIO_HASH=1 -DWITH_VIDEO_HASH=1 .
 # https://stackoverflow.com/questions/22603163/automake-error-ltmain-sh-not-found
 # automake build
 libtoolize --force
-
-# FIXME building in docker will cause `libtoolize` putting `ltmain.sh` into parent folder
-if [[ -n "${DOCKER_BUILDING}" ]]; then
-  mv ../ltmain.sh ./
-fi
-
 aclocal
 autoheader
 autoconf
@@ -23,5 +20,3 @@ automake --add-missing
 # final build
 ./configure
 make && make install
-
-cd ..
